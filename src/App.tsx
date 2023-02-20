cc15eca43a9c3354ec07e976c079b675fbe62bb5
Filip Zolyniak <72789688+Zoltw@users.noreply.github.com>
2023-02-20 18:57:52 +0100

    added dependecies, server, esbuild and starter view

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello on my webpage.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
