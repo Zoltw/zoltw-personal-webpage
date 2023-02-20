0e705f1150d65a7aff1baa6f3d798c82fa72ce09
Filip Zolyniak <72789688+Zoltw@users.noreply.github.com>
2023-02-20 22:42:26 +0100

    added eslint and some cleancode

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# zoltw-personal-webpage
+ <h1 align="center">my personal page</h1>
+ <br/>
+
+ ## Setting up development environment
+
+ ### Requirements:
+ - [Node.js](https://nodejs.org/en/)
+
+ ### Setup
+
+ 1. Make sure that you have `Node.js` installed.
+ 2. Clone the repository
+ ```bash
+ git clone https://github.com/Zoltw/zoltw-personal-webpage.git
+ ```
+ 3. Install dependencies
+ ```bash
+ npm ci
+ ```
+ 4. Start development server
+ ```bash
+ npm start
+ ```
+ 5. Open `http://localhost:4000` in your browser
--- run.js
+++ run.js
@@ -1,51 +1,56 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { build } = require('esbuild');
 const chokidar = require('chokidar');
 const { createServer } = require('http-server');
 const env = process.env.NODE_ENV;
 
 function runServer() {
-    const server = createServer({
-        root: "./public",
-        showDir: true,
-        autoIndex: false,
-        gzip: true,
-        brotli: true,
-    });
-    const PORT = 4000;
-    server.listen(PORT);
-    console.log("You can now view the app in the browser.\n");
-    console.log("   Local:            http://localhost:" + PORT);
-    console.log("   On Your Network:  http://192.168.1.3:" + PORT);
+  const server = createServer({
+    root: './public',
+    showDir: true,
+    autoIndex: false,
+    gzip: true,
+    brotli: true,
+  });
+  const PORT = 4000;
+  server.listen(PORT);
+  console.log('You can now view the app in the browser.\n');
+  console.log('   Local:            http://localhost:' + PORT);
+  console.log('   On Your Network:  http://192.168.1.3:' + PORT);
 }
 
 function runBuild() {
-    build({
-        // stdio: 'inherit',
-        minify: true,
-        entryPoints: ['./src/index.tsx'],
-        outfile: './public/dist/bundle.js',
-        // minify: 'production' === env,
-        bundle: true,
-        format: 'esm',
-        define: {
-            "process.env.NODE_ENV": `'${env}'`
-        },
-        // sourcemap: 'development' === env
-    }).catch((err) => {
-        console.log(err)
-        process.exit(1)
-    });
+  build({
+    // stdio: 'inherit',
+    minify: true,
+    entryPoints: ['./src/index.tsx'],
+    outfile: './public/dist/bundle.js',
+    // minify: 'production' === env,
+    bundle: true,
+    format: 'esm',
+    define: {
+      'process.env.NODE_ENV': `'${env}'`,
+    },
+    // sourcemap: 'development' === env
+  }).catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
 }
 
 chokidar.watch('./src/**', {
-    ignoreInitial: true,
+  ignoreInitial: true,
 }).on('all', (event, path) => {
-    console.log("=> ", event, path);
-    runBuild();
+  console.log('=> ', event, path);
+  runBuild();
 }).on('ready', () => {
-    runServer();
-    runBuild();
-    console.log("\n=====================================");
-    console.log("Esbuild is waiting for changes...");
-    console.log("======================================\n");
+  runServer();
+  runBuild();
+  console.log('\n=====================================');
+  console.log('Esbuild is waiting for changes...');
+  console.log('======================================\n');
+});
+
+process.on('warning', (warning) => {
+  console.log(warning.stack);
 });
