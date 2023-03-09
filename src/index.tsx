9d18567ed3f6d0977f6eb775dcb34d0f67f2bb9b
Filip Zolyniak <72789688+Zoltw@users.noreply.github.com>
2023-03-10 00:38:01 +0100

    fixed finally loading problem

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from '@view/App';
+import Loading from '@components/Loading/Loading';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Suspense fallback={<Loading/>}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>,
 );
--- src/view/App.tsx
+++ src/view/App.tsx
@@ -7,7 +7,6 @@ import './App.css';
 import UpperBar from '@components/Navbars/Upperbar/UpperBar';
 import BottomBar from '@components/Navbars/Bottombar/BottomBar';
 import MichelAngelos from '@components/MichelAngelos/MichelAngelos';
-import Loading from '@components/Loading/Loading';
 import { TechStackSection } from '@components/TechStackSection/TechStackSection';
 import ContainerBox from '@components/ContainerBox/ContainerBox';
 import Input from '@components/Input/Input';
@@ -15,11 +14,9 @@ import Textarea from '@components/TextArea/TextArea';
 import Button from '@components/Button/Button';
 import { About } from '@components/About/About';
 import emailjs from '@emailjs/browser';
-import FileLoader from '@components/FileLoader/FileLoader';
 
 
 export default function App(): JSX.Element {
-  const [loading, setLoading] = useState(true);
   const [titleValid, setTitleValid] = useState(false);
   const [messageValid, setMessageValid] = useState(false);
   const [emailValid, setEmailValid] = useState(false);
@@ -50,46 +47,6 @@ export default function App(): JSX.Element {
 
 
   useEffect(() => {
-    const handleFileLoad = async () => {
-      try {
-        await fetch('public/assets/models/michel_angelos.glb');
-        setLoading(false);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    // if (document.readyState !== 'complete') {
-    handleFileLoad();
-    // }
-  }, []);
-
-  // useEffect(() => {
-  //   const xhr = new XMLHttpRequest();
-
-  //   xhr.onreadystatechange = () => {
-  //     if (xhr.readyState === 4) {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   xhr.open('GET', 'public/assets/models/michel_angelos.glb');
-  //   xhr.send();
-  // }, ['public/assets/models/michel_angelos.glb']);
-
-  useEffect(() => {
-    const onPageLoad = () => {
-      setLoading(true);
-    };
-
-    if (document.readyState === 'complete') {
-      onPageLoad();
-    } else {
-      window.addEventListener('load', onPageLoad);
-      return () => window.removeEventListener('load', onPageLoad);
-    }
-  }, []);
-
-  useEffect(() => {
     setIsFormValid(titleValid && messageValid && emailValid);
   }, [
     titleValid,
@@ -98,104 +55,97 @@ export default function App(): JSX.Element {
   ]);
 
   return (
-    <div >
-      {/* <div className="bg-noise"></div> */}
-      {/* <FileLoader fileUrl='public/assets/models/michel_angelos.glb'/> */}
-      {/* <div onLoad={handleFileLoad}></div> */}
-      {
-        loading ?
-          <Loading /> :
-          // Main wrap
-          <div id="MainWrap">
-            {/* Header */}
-            <header id="header">
-              <UpperBar className={'UpperBar'}/>
-            </header>
-            {/* Main content*/}
-            <div className="MainPage" id={'home'}>
-              <div className="Description">
-                <span className="HelloDescription">Hello.</span>
-                <TypeAnimation
-                  className="ImDescription"
-                  sequence={[
-                    2000,
-                    'I\'m Filip',
-                    1500,
-                    'I\'m Developer',
-                    1500,
-                    'I\'m Designer',
-                    1500,
-                    'I\'m Enthusiast',
-                  ]}
-                  wrapper="div"
-                  cursor={true}
-                  repeat={Infinity}
-                  style={{ fontSize: 'calc(90px + 2vmin)' }}
-                />
-              </div>
-              <Canvas id="three-canvas-container" shadows>
-                <MichelAngelos />
-              </Canvas>
-              <a href="#t">
-                <img className="down-arrow" src="assets/img/arrow-down.svg" alt="" />
-              </a>
-              <span className={'michel-descript'}>Michelangelo (not me)</span>
-            </div>
-            <section className="techStack" id={'t'}>
-              <TechStackSection className={'tech-section'}/>
-            </section>
-            <section className="about" id={'about'}>
-              <About className={'about-section'}/>
-            </section>
-            <section className={'cool-stuff'} id={'stuff'}>
-              <h3>I'm always</h3>
-              <h3>interested</h3>
-              <h3>about cool</h3>
-              <h3>stuff.</h3>
-              <h3>Are you</h3>
-              <h3>minding a</h3>
-              <h3>project?</h3>
-            </section>
-            <section className="contact" id={'contact'}>
-              <div className={'contact-des'}>
-                <h3>Let's connect</h3>
-              </div>
-              <ContainerBox className={'form-box'}>
-                <form className={'form'} ref={refForm} onSubmit={sendEmail}>
-                  <Input
-                    useRef={email}
-                    correctValue={setEmailValid}
-                    type={'email'}
-                    name={'email'}
-                    placeholder={'Your email'}
-                    required
-                    className={'email'}
-                  />
-                  <Input
-                    useRef={title}
-                    correctValue={setTitleValid}
-                    type={'text'}
-                    name={'title'}
-                    placeholder={'Title'}
-                    required
-                    className={'input'}
-                  />
-                  <Textarea
-                    useRef={message}
-                    correctValue={setMessageValid}
-                    name={'message'}
-                    placeholder={'Your message'}
-                    className={'textElement'}
-                  />
-                  <Button text={'send'} type={'submit'}/>
-                </form>
-              </ContainerBox>
-            </section>
-            <footer className={'footer'} id={'footer'}>
-              <BottomBar className={'BottomBar'}/>
-            </footer>
+    <>
+      {/* Main wrap */}
+      <div id="MainWrap">
+        {/* Header */}
+        <header id="header">
+          <UpperBar className={'UpperBar'}/>
+        </header>
+        {/* Main content*/}
+        <div className="MainPage" id={'home'}>
+          <div className="Description">
+            <span className="HelloDescription">Hello.</span>
+            <TypeAnimation
+              className="ImDescription"
+              sequence={[
+                2000,
+                'I\'m Filip',
+                1500,
+                'I\'m Developer',
+                1500,
+                'I\'m Designer',
+                1500,
+                'I\'m Enthusiast',
+              ]}
+              wrapper="div"
+              cursor={true}
+              repeat={Infinity}
+              style={{ fontSize: 'calc(90px + 2vmin)' }}
+            />
           </div>
-      }
-    </div>
+          <Canvas id="three-canvas-container" shadows>
+            <MichelAngelos />
+          </Canvas>
+          <a href="#t">
+            <img className="down-arrow" src="assets/img/arrow-down.svg" alt="" />
+          </a>
+          <span className={'michel-descript'}>Michelangelo (not me)</span>
+        </div>
+        <section className="techStack" id={'t'}>
+          <TechStackSection className={'tech-section'}/>
+        </section>
+        <section className="about" id={'about'}>
+          <About className={'about-section'}/>
+        </section>
+        <section className={'cool-stuff'} id={'stuff'}>
+          <h3>I'm always</h3>
+          <h3>interested</h3>
+          <h3>about cool</h3>
+          <h3>stuff.</h3>
+          <h3>Are you</h3>
+          <h3>minding a</h3>
+          <h3>project?</h3>
+        </section>
+        <section className="contact" id={'contact'}>
+          <div className={'contact-des'}>
+            <h3>Let's connect</h3>
+          </div>
+          <ContainerBox className={'form-box'}>
+            <form className={'form'} ref={refForm} onSubmit={sendEmail}>
+              <Input
+                useRef={email}
+                correctValue={setEmailValid}
+                type={'email'}
+                name={'email'}
+                placeholder={'Your email'}
+                required
+                className={'email'}
+              />
+              <Input
+                useRef={title}
+                correctValue={setTitleValid}
+                type={'text'}
+                name={'title'}
+                placeholder={'Title'}
+                required
+                className={'input'}
+              />
+              <Textarea
+                useRef={message}
+                correctValue={setMessageValid}
+                name={'message'}
+                placeholder={'Your message'}
+                className={'textElement'}
+              />
+              <Button text={'send'} type={'submit'}/>
+            </form>
+          </ContainerBox>
+        </section>
+        <footer className={'footer'} id={'footer'}>
+          <BottomBar className={'BottomBar'}/>
+        </footer>
+      </div>
+    </>
   );
 }
